514d41217e6647cd692a7e738d6dda37915f05f9
Pedro Bayona <pedrobayonn@gmail.com>
2017-12-12 23:29:05 +0100

    Servicio de Firebase listo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -5,13 +5,26 @@ import { Http } from '@angular/http';
 export class InformacionService {
 info:any = {};
 cargada:boolean = false;
+cargada_sobre:boolean = false;
+equipo:any = {};
   constructor( public http:Http ) {
-    this.http.get("assets/data/info.pagina.json").subscribe(data => {
-
-      this.cargada = true;
-      this.info = data.json();
-      console.log(this.info);
-    });
+  this.carga_info();
+  this.carga_sobre_nosotros();
   }
 
+
+public carga_info(){
+  this.http.get("assets/data/info.pagina.json").subscribe(data => {
+    this.cargada = true;
+    this.info = data.json();
+    //console.log(data.json());
+  });
+}
+public carga_sobre_nosotros(){
+  this.http.get("https://paginaweb-86f5a.firebaseio.com/.json").subscribe(data => {
+    this.cargada = true;
+    this.equipo = data.json();
+    console.log(data.json());
+  });
+}
 }
