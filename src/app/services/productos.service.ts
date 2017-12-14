e0a51a65b0f7a18168c0d48b157f70c9174c6e69
Pedro Bayona <pedrobayonn@gmail.com>
2017-12-14 18:55:48 +0100

    Agregamos Firebase al portafolio

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,21 +1,22 @@
 import { Injectable } from '@angular/core';
-import {Http} from '@angular/http';
+import { Http } from '@angular/http';
 
 @Injectable()
 export class ProductosService {
 
-  productos:any[] = [];
-  cargando:boolean = true;
-  constructor( private http:Http) {
+  productos: any[] = [];
+  cargando: boolean = true;
+  constructor(private http: Http) {
     this.cargar_productos();
-   }
+  }
 
-  public cargar_productos(){
+  public cargar_productos() {
     this.cargando = true;
-      this.http.get('https://paginaweb-86f5a.firebaseio.com/productos_idx.json')
-      .subscribe(res =>{
+    this.http.get('https://paginaweb-86f5a.firebaseio.com/productos_idx.json')
+      .subscribe(res => {
         console.log(res.json());
         this.cargando = false;
+        this.productos = res.json();
       })
   }
 }
